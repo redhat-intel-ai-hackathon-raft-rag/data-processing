50e80c074e5aa71793a240d2ffd487f9b6bcdbd1
eichi_uehara <eichi.uehara@aflo.one>
2024-10-17 10:21:12 +0900

    fix: raft

--- dataset/raft/chunks_to_dataset.py
+++ dataset/raft/chunks_to_dataset.py
@@ -16,12 +16,25 @@ def chunks_to_dataset(chunks: str, distuctor_only_dataset_ratio=0.2) -> List[dic
             for part in parts:
                 j_array = generate_question_answer_set(part)
                 for j in j_array:
-                    d = {
-                        "instruction": j["question"],
-                        "input": chunk.page_content,
-                        "chosen": j["answer"],
-                        "rejected": ""
-                    }
+                    dice = random.randint(0, 1)
+                    if dice > distuctor_only_dataset_ratio and num_chunks > 1:
+                        d = {
+                            "instruction": j["question"],
+                            "input": "\n".join(str(d) for d in distructors_generator(chunks, idx, num_distructors=3)),
+                            "output": j["answer"]
+                        }
+                    elif dice <= distuctor_only_dataset_ratio and num_chunks > 1:
+                        d = {
+                            "instruction": j["question"],
+                            "input": "\n".join(str(d) for d in distructors_generator(chunks, idx, num_distructors=3)),
+                            "output": j["answer"]
+                        }
+                    else:
+                        d = {
+                            "instruction": j["question"],
+                            "input": chunk.page_content,
+                            "output": j["answer"]
+                        }
                     dataset.append(d)
             continue
         j_array = generate_question_answer_set(chunk.page_content)
