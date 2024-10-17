f89e04d62a9cb29d7997655d3a223c5f6d6b04ac
eichi_uehara <eichi.uehara@aflo.one>
2024-10-17 10:38:12 +0900

    fix: script

--- dataset/raft.sh
+++ dataset/raft.sh
@@ -1,13 +1,24 @@
 output_folder="dataset/generated_dataset"
-## for all file in raw_dataset with extension .json, .csv, .txt, .pdf
-find raw_dataset -type f -name "*.json" -o -name "*.csv" -o -name "*.txt" -o -name "*.pdf" | while read file; do
-    # for json
-    if [ ${file: -5} == ".json" ]; then
-    python -m dataset.raft.raft --doc_type json --text_field_name text --file_path $file --output_folder $output_folder
-    ## for pdf file
-    if [ ${file: -4} == ".pdf" ]; then
-    python -m dataset.raft.raft --doc_type pdf --file_path {str} --output_folder $output_folder
-    ## for text file
-    if [ ${file: -4} == ".txt" ]; then
-    python -m dataset.raft.raft --doc_type text --file_path {str} --output_folder $output_folder
+
+# Find all files in raw_dataset with specified extensions
+find dataset/raw_dataset -type f \( -name "*.json" -o -name "*.csv" -o -name "*.txt" -o -name "*.pdf" \) | while read -r file; do
+    # Check for json files
+    if [[ ${file: -5} == ".json" ]]; then
+        python -m dataset.raft.raft --doc_type json --text_field_name text --file_path "$file" --output_folder "$output_folder"
+    fi
+
+    # Check for pdf files
+    if [[ ${file: -4} == ".pdf" ]]; then
+        python -m dataset.raft.raft --doc_type pdf --file_path "$file" --output_folder "$output_folder"
+    fi
+
+    # Check for text files
+    if [[ ${file: -4} == ".txt" ]]; then
+        python -m dataset.raft.raft --doc_type text --file_path "$file" --output_folder "$output_folder"
+    fi
+
+    # Check for csv files
+    if [[ ${file: -4} == ".csv" ]]; then
+        python -m dataset.raft.raft --doc_type csv --file_path "$file" --output_folder "$output_folder"
+    fi
 done
